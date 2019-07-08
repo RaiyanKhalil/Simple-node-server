16e0db61a3a87a2ead27b9632574f768a15b5201
RaiyanKhalil <raiyankhalil@gmail.com>
2019-07-08 09:44:09 +0600

    classwork 8b

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
   var http = require('http');
-  var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function(err, data){
-      if(err){
-        return console.log("File read error");
-      }
-      res.end(data);
-    });
+  var express = require('express');
+  var app = express();
+  var server = http.Server(app);
+  
+  // var server = http.createServer(function(req, res){
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/html');
+  //   fs.readFile('index.html', function(err, data){
+  //     if(err){
+  //       return console.log("File read error");
+  //     }
+  //     res.end(data);
+  //   });
 
-  });
+  // });
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html')
+  })
+
+  app.get('/second', function(req, res){
+    res.sendFile(__dirname+'/second.html')
+  })
+
   server.listen(3000, "localhost", function(){
     console.log('Server running');
   });
